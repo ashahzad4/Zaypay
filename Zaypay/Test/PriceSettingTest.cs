e25a354a931466b05731ea9a9fcad0941f600592
ashahzad4 <ashahzad4@gmail.com>
2012-10-03 12:04:01 +0200

    basic zaypay class library files

---
+++ Zaypay/Test/PriceSettingTest.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using NSubstitute;
+using Zaypay;
+using Zaypay.WebService;
+using System.Collections;
+
+
+namespace Zaypay.Test
+{
+
+    [TestFixture]
+    class PriceSettingTest
+    {
+
+        private Hashtable expectedHash;
+        private PriceSetting ps;
+
+        [SetUp]
+        protected void SetUp()
+        {
+            
+            expectedHash = new Hashtable();
+            ps = Substitute.For<PriceSetting>(111, "thekey");
+            ps.PAYALOGUE_ID = 111;
+            ps.LOCALE = "nl-NL";
+            ps.PAYMENT_METHOD_ID = 2;
+
+
+        }
+        
+        [Test]
+        public void LocalForIP()
+        {
+
+            expectedHash["locale"] = "nl-NL";
+            ps.GetResponse("https://secure.zaypay.com/82.94.123.123/pay/111/locale_for_ip?key=thekey").Returns(expectedHash);
+
+            
+            LocalForIPResponse actualResponse = new LocalForIPResponse(expectedHash);
+
+            CollectionAssert.AreEquivalent(actualResponse.RESPONSE, expectedHash);
+            //Assert.AreEqual("nl-NL", ps.LocaleForIP("82.94.123.123"));
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(Exception))]
+
+        public void LocaleForIPWithException()
+        {
+            ps.LocaleForIP("");
+
+        }
+
+        [Test]        
+        public void ListLocales()
+        {
+
+            ps.GetResponse("https://secure.zaypay.com//pay/111/list_locales?key=thekey").Returns(expectedHash);
+
+            ListLocalesResponse actualResponse = ps.ListLocales();
+            CollectionAssert.AreEquivalent(actualResponse.RESPONSE, expectedHash);
+
+            ChangeHashtable(ref expectedHash);
+
+            ps.GetResponse("https://secure.zaypay.com/123/pay/111/list_locales?key=thekey").Returns(expectedHash);
+            actualResponse = null;
+            actualResponse = ps.ListLocales(123);
+            CollectionAssert.AreEquivalent(actualResponse.RESPONSE, expectedHash);
+
+            
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(Exception))]
+
+        public void ListLocaleWithException()
+        {
+            ps.ID = 0;
+            ps.KEY = "";
+            ps.ListLocales();
+        }
+        
+
+        private void ChangeHashtable(ref Hashtable hash)
+        {
+            hash = null;
+            hash = new Hashtable();
+            hash["amount"] = "123";
+        }
+
+    }
+}
+
---
+++ Zaypay/WebService/HttpRequestResp.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Xml;
+using System.Collections;
+using System.Xml.Linq;
+using Zaypay;
+using Zaypay.Utility;
+
+
+namespace Zaypay.WebService
+{
+    public class HttpRequestResp
+    {
+        private string URI;
+        private string Request;
+        private string RequestMethod = "GET";
+
+
+        public HttpRequestResp(string pURI, string reqMethod = "GET", string pRequest = "")
+        {
+            Request = pRequest;
+            URI = pURI;
+            RequestMethod = reqMethod;
+        }
+
+        public Hashtable GetResponse()
+        {
+
+            //HttpWebResponse resp = SendRequest();
+            ////XmlTextReader reader = new XmlTextReader(resp.GetResponseStream());
+
+            //StreamReader reader = new StreamReader(resp.GetResponseStream());
+
+            //// Read the whole contents and return as a string  
+            //string result = reader.ReadToEnd();
+            //Console.WriteLine("TTTTTTTTT: " + result);
+            //System.Diagnostics.Debug.WriteLine("TTTTTTT: " + result);
+
+            //while (reader.Read())
+            //{
+
+
+            //    switch (reader.NodeType)
+            //    {
+            //        case XmlNodeType.Element: // The node is an Element.
+            //            Console.Write("<" + reader.Name);
+
+            //            while (reader.MoveToNextAttribute()) // Read attributes.
+            //                Console.Write(" " + reader.Name + "='" + reader.Value + "'");
+            //            Console.Write(">");
+            //            break;
+            //        case XmlNodeType.Text: //Display the text in each element.
+            //            Console.WriteLine(reader.Value);
+            //            break;
+            //        case XmlNodeType.EndElement: //Display end of element.
+            //            Console.Write("</" + reader.Name);
+            //            Console.WriteLine(">");
+            //            break;
+            //    }
+            //}
+
+            HttpWebResponse resp2 = SendRequest();
+            XmlTextReader reader2 = new XmlTextReader(resp2.GetResponseStream());
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(reader2);
+            XmlNode main = doc.DocumentElement;
+
+            Hashtable htMain = XMLParser.ParseNode(main);
+
+            return htMain;
+        }
+
+        public HttpWebResponse SendRequest()
+        {
+
+            HttpWebRequest webrequest = (HttpWebRequest)WebRequest.Create(URI);
+
+            webrequest.Method = RequestMethod;
+            webrequest.ContentType = "application/x-www-form-urlencoded";
+            webrequest.Accept = "application/xml";
+
+            if (RequestMethod == "POST")
+            {
+                BuildReqStream(ref webrequest);
+            }
+
+            return (HttpWebResponse)webrequest.GetResponse();
+
+
+        }
+
+        private void BuildReqStream(ref HttpWebRequest webrequest)
+        {
+            
+            byte[] bytes = Encoding.ASCII.GetBytes(Request);
+            
+            webrequest.ContentLength = bytes.Length;
+
+            Stream oStreamOut = webrequest.GetRequestStream();
+            oStreamOut.Write(bytes, 0, bytes.Length);
+            oStreamOut.Close();
+        }
+
+    }
+}
